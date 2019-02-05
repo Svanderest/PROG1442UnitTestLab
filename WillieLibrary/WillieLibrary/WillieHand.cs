27726667bba05583db999ec8e6751f741270fe16
Niagara College <helpdesk@niagaracollege.ca>
2019-02-05 15:51:38 -0500

    initial commit

---
+++ WillieLibrary/WillieLibrary/WillieHand.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WillieLibrary
+{
+    public partial class WillieHand
+    {
+        private int _CardOne;
+        private int _CardTwo;
+        //Commented out redundant private field
+        //private int _Willie;
+
+        //Made second arguement nullable to allow for part B
+        public WillieHand(int CardOne, int? CardTwo = null)
+        {
+            _CardOne = CardOne;
+            _CardTwo = (int)(CardTwo ?? 0);
+        }
+
+        //Bugged code provided
+        //public int Willie
+        //{
+        //    get
+        //    {
+        //        if (_CardOne > 10)
+        //            _Willie = 10;
+        //        else
+        //            _Willie = _CardOne;
+        //        if (_CardTwo > 10)
+        //            _Willie = 10;
+        //        else
+        //            _Willie = _CardTwo;
+        //        return _Willie;
+        //    }
+        //}
+
+        //Actual Code
+        public int Willie
+        {
+            get
+            {
+                return (_CardOne > 10 ? 10 : _CardOne) + (_CardTwo > 10 ? 10 : _CardTwo);
+            }
+        }
+
+        public int CardOne
+        {
+            get
+            {
+                return _CardOne;
+            }
+        }
+
+        public int CardTwo
+        {
+            get
+            {
+                return _CardTwo;
+            }
+        }
+    }
+}
---
+++ WillieLibrary/WillieLibraryTests/WillieHandTests.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WillieLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WillieLibrary.Tests
+{
+    [TestClass()]
+    public class WillieHandTests
+    {
+        //Part A
+        [TestMethod()]
+        public void NineJackTest()
+        {
+            //Arrange
+            int one = 9;
+            int two = 11;
+            WillieHand target = new WillieHand(one, two);
+
+            //Act
+            int expected = 19;
+            int actual = target.Willie;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void AceQueenTest()
+        {
+            //Arrange
+            int one = 1;
+            int two = 12;
+            WillieHand target = new WillieHand(one, two);
+
+            //Act
+            int expected = 11;
+            int actual = target.Willie;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void KingJackTest()
+        {
+            //Arrange
+            int one = 13;
+            int two = 11;
+            WillieHand target = new WillieHand(one, two);
+
+            //Act
+            int expected = 20;
+            int actual = target.Willie;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void SevenTwoTest()
+        {
+            //Arrange
+            int one = 7;
+            int two = 2;
+            WillieHand target = new WillieHand(one, two);
+
+            //Act
+            int expected = 9;
+            int actual = target.Willie;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void QueenNineTest()
+        {
+            //Arrange
+            int one = 12;
+            int two = 9;
+            WillieHand target = new WillieHand(one, two);
+
+            //Act
+            int expected = 19;
+            int actual = target.Willie;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void TenKingTest()
+        {
+            //Arrange
+            int one = 10;
+            int two = 13;
+            WillieHand target = new WillieHand(one, two);
+
+            //Act
+            int expected = 20;
+            int actual = target.Willie;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        //Part B
+        [TestMethod()]
+        public void JackTest()
+        {
+            //Arrange
+            int one = 11;
+            WillieHand target = new WillieHand(one);
+
+            //Act
+            int expected = 10;
+            int actual = target.Willie;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void TenTest()
+        {
+            //Arrange
+            int one = 10;
+            WillieHand target = new WillieHand(one);
+
+            //Act
+            int expected = 10;
+            int actual = target.Willie;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void NineTest()
+        {
+            //Arrange
+            int one = 9;
+            WillieHand target = new WillieHand(one);
+
+            //Act
+            int expected = 9;
+            int actual = target.Willie;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void AceTest()
+        {
+            //Arrange
+            int one = 1;
+            WillieHand target = new WillieHand(one);
+
+            //Act
+            int expected = 1;
+            int actual = target.Willie;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
